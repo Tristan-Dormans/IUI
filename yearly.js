77f02274b42da97857b0e258083f85ba92b1a8bc
Tristan <62807768+Tristan-Dormans@users.noreply.github.com>
2024-10-17 17:51:38 +0200

    Fixed yearly.js

--- yearly.js
+++ yearly.js
@@ -11,7 +11,7 @@ months.forEach((month, index) => {
   monthDiv.textContent = month;
 
   monthDiv.addEventListener('click', () => {
-    const year = new Date().getFullYear();
+    const year = new Date().getFullYear() + 1;
     const day = new Date(year, index, 1).getDate();
     const date = `${day < 10 ? `0${day}` : day}/${index + 1 < 10 ? `0${index + 1}` : index + 1}/${year}`;
     window.location.href = `./monthly.html?${date}`;
